70616f549ca2b723686665eb6699983432203a7a
Nishant <nishantsharma15012005abcd@gmail.com>
2024-10-27 21:58:38 +0530

    testing

--- public/js/myProfile.js
+++ public/js/myProfile.js
@@ -1,22 +1,10 @@
-console.log("hello");
+
 let more=document.querySelector(".icon");
-console.log("hello");
 let options=document.querySelector(".options");
 more.addEventListener("click",()=>{
-    console.log("inside");
     if(options.style.visibility=="hidden"||options.style.visibility==""){
         options.style.visibility="visible";            
     }else{
         options.style.visibility="hidden";      
     }
 });
-// more.addEventListener("click", () => {
-//     if (options.style.display === "none") {
-//         options.style.display = "block";
-//     } else {
-//         options.style.display = "none";
-//     }
-// });
-
-console.log(options);
-console.log(more);
